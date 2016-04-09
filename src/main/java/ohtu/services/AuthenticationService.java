8a30e4362346b15199fde40ec405e1d3010def6a
Denopia <miika.leinonen@helsinki.fi>
2016-04-10 02:47:31 +0300

    checkstyle

--- src/main/java/ohtu/services/AuthenticationService.java
+++ src/main/java/ohtu/services/AuthenticationService.java
@@ -17,14 +17,15 @@ public class AuthenticationService {
         this.userDao = userDao;
     }
 
+    public boolean logInSub(String username, String password, User user){
+        return user.getUsername().equals(username)&& user.getPassword().equals(password);
+    }
     public boolean logIn(String username, String password) {
         for (User user : userDao.listAll()) {
-            if (user.getUsername().equals(username)
-                    && user.getPassword().equals(password)) {
+            if (logInSub(username, password, user)) {
                 return true;
             }
         }
-
         return false;
     }
 
@@ -32,53 +33,57 @@ public class AuthenticationService {
         if (userDao.findByName(username) != null) {
             return false;
         }
-
         if (invalid(username, password)) {
             return false;
         }
-
         userDao.add(new User(username, password));
-
         return true;
     }
 
+    private boolean checkNameSub(String username, String letters, int j){
+        boolean ok = false;
+        for (int i = 0; i < letters.length(); i++) {
+            if (username.toLowerCase().charAt(j) == letters.charAt(i)) ok = true;       
+        }
+        return !ok;
+    }
+    private boolean checkName(String username, String letters, int j) {
+        while (j < username.length()) {
+            if(checkNameSub(username, letters, j)) return true;
+            j++;
+        }return false;
+    }
+    
+    private boolean checkPasswordSub(String password, String letters, int u, int k){
+        boolean ok = false;
+        for (int i = 0; i < letters.length(); i++) {
+            if (password.toLowerCase().charAt(k) == letters.charAt(i)) ok = true;    
+        }
+        return ok;
+    }
+
+    private boolean checkPassword(String password, String letters, int u, int k) {
+        while (k < password.length()) {     
+            if (checkPasswordSub(password, letters, u, k))u++;     
+            k++;
+        }return u == password.length();
+    }
+
     private boolean invalid(String username, String password) {
-        // validity check of username and password
         String letters = "abcdefghijklmnopqrstuvwxyz";
-        if (username.length() < 3) {
+        if(checkNameValidity(username, letters)){
             return true;
         }
-        if (password.length() < 8) {
-            return true;
-        }
-        for (int j = 0; j < username.length(); j++) {
-            boolean ok = false;
-            for (int i = 0; i < letters.length(); i++) {
-                if (username.toLowerCase().charAt(j) == letters.charAt(i)) {
-                    ok = true;
-                }
-            }
-            if (!ok) {
-                return true;
-            }
-        }
-        int u = 0;
-        for (int j = 0; j < password.length(); j++) {
-            boolean ok = false;
-
-            for (int i = 0; i < letters.length(); i++) {
-                if (password.toLowerCase().charAt(j) == letters.charAt(i)) {
-                    ok = true;
-                }
-            }
-            if (ok) {
-                u++;
-            }
-        }
-        if (u == password.length()) {
-            return true;
-        }
-
-        return false;
+        return checkPasswordValidity(password, letters);
+    }
+    
+    private boolean checkNameValidity(String username, String letters){
+        if (username.length() < 3)return true;
+        return checkName(username, letters, 0);
+    }
+    
+    private boolean checkPasswordValidity(String password, String letters){
+        if (password.length() < 8)return true;
+        return checkPassword(password, letters, 0, 0);
     }
 }
